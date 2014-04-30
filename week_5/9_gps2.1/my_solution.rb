3dfe781f9e61bdde338ec9fb3c272a4cd052f3fd
Armen Vartan <vartan.armen@gmail.com>
2014-04-30 16:23:07 -0400

    gps 2.1 solution

--- week_5/9_gps2.1/my_solution.rb
+++ week_5/9_gps2.1/my_solution.rb
@@ -1,24 +1,39 @@
 # U2.W5: Bakery Challenge GPS
 
-# I worked on this challenge with: 
+# I worked on this challenge with: Emily Anderson
+
+def bakery_num(num_of_people, fav_food)
+  my_list = {"pie" => 8, "cake" => 6, "cookie" => 1}
+  pie_qty = 0
+  cake_qty = 0
+  cookie_qty = 0
+  # Error if the fav_food input isn't sold at the bakery
+  raise ArgumentError.new("You can't make that food") unless my_list.has_key?(fav_food)
+  order_qty = num_of_people / my_list[fav_food] # the number of fav_food that you're going to make
+  people_left = num_of_people % my_list[fav_food] # the number of people left to serve afterwards
+
+  if people_left == 0 # in the case that everyone can have their favorite food
+    return "You need to make #{order_qty} #{fav_food}(s)."
+  else
+    case fav_food #for all other cases
+    when "pie" #when pie is your favorite food and there are leftover people to serve
+      pie_qty = order_qty
+      cake_qty = people_left / my_list["cake"] # this will be 0 if there are less than 6 people to serve
+      cookie_qty = people_left % my_list["cake"] # all the leftovers will be served cookies. in the case that there are less than 6 poeple to start with, this still works
+    when "cake" # there is no need to mention pies in this case
+      cake_qty = order_qty
+      cookie_qty = people_left
+    when "cookie" #same logic as the "cake" case
+      cookie_qty = order_qty
+    end
+    return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
+  end
+end
 
 
-
-
-
-
-# Our Refactored Solution
-
-
-
-
-
-
-
-
-
+#-----------------------------------------------------------------------------------------------------
 #DRIVER CODE-- DO NOT MODIFY ANYTHING BELOW THIS LINE (except in the section at the bottom)
-# These are the tests to ensure it's working. 
+# These are the tests to ensure it's working.
 # These should all print true if the method is working properly.
 p bakery_num(24, "cake") == "You need to make 4 cake(s)."
 p bakery_num(41, "pie") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)."
@@ -28,12 +43,11 @@ p bakery_num(130, "pie") == "You need to make 16 pie(s), 0 cake(s), and 2 cookie
 # p bakery_num(3, "apples") # this will raise an ArgumentError
 
 # You SHOULD change this driver code. Why? Because it doesn't make sense.
-p bakery_num(41, "cake") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
+p bakery_num(41, "cake") == "You need to make 0 pie(s), 6 cake(s), and 5 cookie(s)."
+p bakery_num(24, "cakey")
 
 
 
 
-#  Reflection 
 
-
-
+# Our Refactored Solution
