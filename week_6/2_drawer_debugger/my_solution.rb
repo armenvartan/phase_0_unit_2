9e7265ce86e06baf2be4572ba7f553f386c8564f
Armen Vartan <vartan.armen@gmail.com>
2014-05-05 16:57:20 -0400

    Up to debugging line 58

--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -8,55 +8,60 @@
 
 class Drawer
 
-attr_reader :contents
+  attr_reader :contents
 
-# Are there any more methods needed in this class?
+  # Are there any more methods needed in this class?
 
-def initialize
-  @contents = []
-  @open = true
+  def initialize
+    @contents = []
+    @open = true
+  end
+
+  def open
+  @open   = true
+  end
+
+  def close
+    @open = false
+  end
+
+  def add_item(item)
+    @contents << item
+  end
+
+  def remove_item(*item) #what is `#pop` doing?
+    puts "You didn't give an item to remove" if item == nil
+    @contents.delete(item)
+  end
+
+  def dump  # what should this method return?
+    puts "Your drawer is empty."
+  end
+
+  def view_contents
+    puts "The drawer contains:"
+    @contents.each {|silverware| puts "- " + silverware.type }
+  end
 end
-
-def open
-@open   = true
-end
-
-def close
-  @open = false
-end
-
-def add_item
-  @contents << item
-end
-
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-  @contents.delete(item)
-end
-
-def dump  # what should this method return?
-  puts "Your drawer is empty."
-end
-
-def view_contents
-  puts "The drawer contains:"
-  @contents.each {|silverware| puts "- " + silverware.type }
-end
-
 
 class Silverware
   attr_reader :type
 
-# Are there any more methods needed in this class?
+  # Are there any more methods needed in this class?
 
-def initialize(type, clean = true)
-  @type = type
-  @clean = clean
-end
+  def initialize(type, clean = true)
+    @type = type
+    @clean = clean
+  end
 
-def eat
-  puts "eating with the #{type}"
-  @clean = false
-end
+  def eat
+    puts "eating with the #{type}"
+    @clean = false
+  end
+
+  def clean_silverware
+    @clean = true
+  end
 
 end
 
